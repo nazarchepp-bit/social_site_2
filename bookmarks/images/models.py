192e912bb0db61a0e82785e2e602312091e7a157
nazar <dsdbsdh@mail.ru>
2025-10-09 21:31:46 +0500

    Initial commit without database

---
+++ bookmarks/images/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
+from django.urls import reverse
+import uuid
+
+class Image(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='images_created',
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, blank=True)
+    url = models.URLField(max_length=2000)
+    image = models.ImageField(upload_to='images/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    created = models.DateField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            # Генерируем slug из title
+            self.slug = slugify(self.title)
+            # Если slug все равно пустой (например, title состоит из спецсимволов),
+            # генерируем уникальный slug
+            if not self.slug:
+                self.slug = f"image-{uuid.uuid4().hex[:8]}"
+        super().save(*args, **kwargs)
+
+    users_like = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='images_liked',
+        blank=True
+    )
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
---
+++ bookmarks/images/views.py
@@ -0,0 +1,101 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from .forms import ImageCreateForm
+from django.shortcuts import get_object_or_404
+from .models import Image
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:  # Правильное исключение
+            pass
+    return JsonResponse({'status': 'error'})
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(
+        request,
+        'images/image/detail.html',
+        {
+            'section': 'images',
+            'image': image
+        }
+    )
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        # форма отправления
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            # если данные формы валидны
+            cd = form.cleaned_data
+            new_image = form.save(commit=False)
+            # назначить текущего пользователя
+            new_image.user = request.user
+            new_image.save()
+            messages.success(
+                request,
+                'Image added successfully'
+            )
+            # перенаправить к представлению детальной
+            # информации о только что созданном элементе
+            return redirect(new_image.get_absolute_url())
+    else:
+        # скомпоновать форму с данными,
+        # предоставленными букмарклетом методом GET
+        form = ImageCreateForm(data=request.GET)
+    return render(
+        request,
+        'images/image/create.html',
+        {
+            'section': 'images',
+            'form': form
+        }
+    )
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # если страница не является целым число, то доставить первую страницу
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            # Если AJAX-запрос и страница вне диапазона,
+            # то вернуть пустую страницу
+            return HttpResponse('')
+        # Если страница вне диапазона,
+        # то вернуть последнюю страницу результатов
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(
+            request,
+            'images/image/list_images.html',
+            {'section': 'images', 'images': images}
+        )
+    return render(
+        request,
+        'images/image/list.html',
+        {'section': 'images', 'images': images}
+    )
